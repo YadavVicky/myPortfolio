5f46b33afc891ee213ec8a961aeca51caf48dc6a
YadavVicky <50552164+YadavVicky@users.noreply.github.com>
2023-07-22 09:28:37 +0530

    Added new Download Link for Resume

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LandingComponent implements OnInit {
 
   my_linked_in = 'https://www.linkedin.com/in/vinay-kumar-yadav-14b87b192';
-  resume_download_link = 'https://drive.google.com/file/d/1FIuxy9i2wl_JopXDw2ndrIU9k2M6ikJR/view?usp=sharing';
+  resume_download_link = 'https://drive.google.com/file/d/1phkcfmLa-1pG-EQ_Y9qQCwfS5mrGfgmv/view?usp=sharing';
   constructor() { }
 
   ngOnInit(): void {
